bd675d2a14dbbf6b9aec452871d682d8ebfe9faf
Flavio Silva <flavio1110@gmail.com>
2023-03-27 21:35:04 +0200

    enable force update

--- cmd/push.go
+++ cmd/push.go
@@ -24,14 +24,18 @@ got push`,
 		repo, err := git.PlainOpen(path)
 		exitIfError(err)
 
+		force := len(args) > 0 && args[0] == "f"
+
 		err = repo.Push(&git.PushOptions{
 			RemoteName: "origin",
+			Force:      force,
 		})
 		if err != nil {
 			if errors.Is(err, git.NoErrAlreadyUpToDate) {
 				fmt.Println("Nothing to push. Remote is already up to date!")
 				return
 			}
+
 			exitIfError(err)
 		}
 		ref, err := repo.Head()
